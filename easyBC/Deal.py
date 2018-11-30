1b9897ed55131ae5e170edcf5c32e7a645c8dec5
xuzhipeng <xuzhipenganhui@hotmail.com>
2018-11-30 14:55:35 +0800

    2018-11-30

--- easyBC/Deal.py
+++ easyBC/Deal.py
@@ -1,16 +1,7 @@
 import pymysql.cursors
 
 class Deal(object):
-    cur_capital = 0.00
-    cur_hold = 0.00
-    cur_money_rest = 0.00
-    stock_pool = []
-    stock_map1 = {}
-    stock_map2 = {}
-    stock_map3 = {}
-    stock_all = []
-    ban_list = []
-
+    """持仓和账户情读取器，每次交易或者更新行情时更新"""
     def __init__(self,state_dt):
         # 建立数据库连接
         db = pymysql.connect(host="localhost", user='root', passwd='8261426', db='stock', charset='utf8')
@@ -30,16 +21,11 @@ class Deal(object):
 
             else:
                 print("账户表初始化未成功")
-
+            ##读取position数据
             sql_select2 = 'select * from my_position a where a.date = %s' %repr(state_dt)
             cursor.execute(sql_select2)
             done_set2 = cursor.fetchall()
-            self.stock_pool = []
-            self.stock_all = []
-            self.stock_map1 = []
-            self.stock_map2 = []
-            self.stock_map3 = []
-            self.ban_list = []       ##禁止买入池
+
             if len(done_set2) > 0:
                 self.stock_pool = [x[0] for x in done_set2]
                 self.stock_cost_price = {x[0]: float(x[1]) for x in done_set2}  #买入价格
@@ -48,6 +34,16 @@ class Deal(object):
                 self.stock_amount = {x[0]: int(x[4]) for x in done_set2}  # 持仓金额
                 self.stock_margin = {x[0]: int(x[5]) for x in done_set2}  # 保证金
                 self.stock_side = {x[0]: int(x[6]) for x in done_set2}  # 持仓方向
+
+            else:
+                self.stock_pool = ["cash"]
+                self.stock_cost_price = {"cash":1}  # 买入价格
+                self.stock_revenue = {"cash":0}  # 利润
+                self.stock_volume = {"cash":self.cur_available_fund}  # 持仓数量
+                self.stock_amount = {"cash":self.cur_available_fund}  # 持仓金额
+                self.stock_margin = {"cash":0}  # 保证金
+                self.stock_side = {"cash":"buy"}  # 持仓方向
+                self.ban_list = []  ##禁止买入池
 
             # sql_select3 = 'select * from ban_list'
             # cursor.execute(sql_select3)
